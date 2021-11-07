60b87df0c786826d8d51a4ac4cbbd08b5c0f196e
Armaan Kara <vancouverarmaan@gmail.com>
2021-11-07 11:30:44 -0800

    added in client side

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const querystring = require('querystring');
 const axios = require('axios');
+const { query } = require('express');
 
 const app = express();
 const port = 8888;
@@ -65,35 +66,19 @@ app.get('/callback', (req, res) => {
   })
     .then(response => {
       if (response.status === 200) {
+        const { access_token, refresh_token } = response.data;
+        const queryParams = querystring.stringify({
+          access_token,
+          refresh_token,
+        })
 
-        // const { access_token, token_type } = response.data;
+        //redirect to react app
+        res.redirect(`http://localhost:3000/?${queryParams}`)
+        //pass along tokens in query params
 
-        // axios.get('https://api.spotify.com/v1/me', {
-        //   headers: {
-        //     Authorization: `${token_type} ${access_token}`
-        //   }
-        // })
-        //   .then(response => {
-        //     res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
-        //   })
-        //   .catch(error => {
-        //     res.send(error);
-        //   });
-
-
-        //For testing route handler
-        const { refresh_token } = response.data;
-
-        axios.get(`http://localhost:8888/refresh_token?refresh_token=${refresh_token}`)
-          .then(response => {
-            res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
-          })
-          .catch(error => {
-            res.send(error);
-          });
 
       } else {
-        res.send(response);
+        res.redirect(`/?${querystring.stringify({ error: 'invalid token'})}`);
       }
     })
     .catch(error => {
